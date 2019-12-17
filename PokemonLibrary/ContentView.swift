aaa6c5eab566145f179394cc10ba3b5cb7c9f508
MIKE432 <michal.raszczuk.apusoft@gmail.com>
2019-12-17 18:29:32 +0100

    Initial Commit

---
+++ PokemonLibrary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokemonLibrary
+//
+//  Created by Michał Raszczuk on 17/12/2019.
+//  Copyright © 2019 Michał Raszczuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
